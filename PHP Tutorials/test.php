c8833a269384b79f9a4cab288bc78196f5ea6b84
antsar <antsar@ttu.ee>
2019-12-29 19:14:04 +0200

    second

---
+++ PHP Tutorials/test.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "Hello world";
+
+// This is a single line comment
+
+/* This is a section
+   of multiline comments
+   that will not be interpreted */
+
+$mycounter = 1;
+$mystring = "Hello";
+$myarray = array("one", "two", "three");
+
+echo $mycounter;
+echo PHP_EOL;
+echo $mystring + PHP_EOL;
+echo $myarray;
